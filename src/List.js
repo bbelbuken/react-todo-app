ce9c1f09292c06a458ba2aa496a6b1db0edd489f
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-08 23:50:49 +0300

    mapped data to components

---
+++ src/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListItem from "./ListItem";
+
+const List = () => {
+  return (
+    <div>
+      <ul>
+<ListItem />
+      </ul>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/ListItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListItem = () => {
+  return (
+    <li>A Item</li>
+  )
+}
+
+export default ListItem
