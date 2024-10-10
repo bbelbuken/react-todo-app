af89cbd81ee0d06e19e515c75ad1e847f51f037a
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-10 19:57:36 +0300

    data added, fetching complete

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import { useState, useEffect } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import List from "./List";
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
+  const API_URL = "http://localhost:3500/items";
   const [inputValue, setInputValue] = useState("");
-  const [listItem, setListItem] = useState(
-    JSON.parse(localStorage.getItem("todolist")) || []
-  );
+  const [listItem, setListItem] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    localStorage.setItem("todolist", JSON.stringify(listItem));
-  }, [listItem]);
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error("Did not receive data");
+        const listItems = await response.json();
+        setListItem(listItems);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError(error.msg);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchItems();
+    }, 1000);
+  }, []);
 
   const addItem = (item) => {
     const id = listItem.length ? listItem[listItem.length - 1].id + 1 : 1;
@@ -41,18 +58,26 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header />
-      <Form
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        handleSubmit={handleSubmit}
-      />
-      <List
-        listItem={listItem}
-        deleteItem={deleteItem}
-        handleCheck={handleCheck}
-      />
+    <div>
+      {isLoading ? (<>
+        <CircularProgress className="backdrop" />
+        <p>Receiving Data</p>
+      </>
+      ) : (
+        <div className="container">
+          <Header />
+          <Form
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            handleSubmit={handleSubmit}
+          />
+          <List
+            listItem={listItem}
+            deleteItem={deleteItem}
+            handleCheck={handleCheck}
+          />
+        </div>
+      )}
     </div>
   );
 }
