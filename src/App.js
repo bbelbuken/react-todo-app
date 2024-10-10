4e248341fc7744d93ceb79bbcb335c3b0bc94fc9
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-10 19:21:10 +0300

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import List from "./List";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [listItem, setListItem] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
+  const [listItem, setListItem] = useState(
+    JSON.parse(localStorage.getItem("todolist")) || []
+  );
 
-  const setAndSaveItem = (newItem) => {
-    setListItem(newItem)
-    localStorage.setItem("todolist", JSON.stringify(newItem))
-
-  }
+  useEffect(() => {
+    localStorage.setItem("todolist", JSON.stringify(listItem));
+  }, [listItem]);
 
   const addItem = (item) => {
-    const id = listItem.length ? listItem[listItem.length - 1].id + 1 : 1;    
+    const id = listItem.length ? listItem[listItem.length - 1].id + 1 : 1;
     const newItem = { id, checked: false, item };
     const addToList = [...listItem, newItem];
-    setAndSaveItem(addToList);
-  
+    setListItem(addToList);
   };
 
   const deleteItem = (id) => {
-    const filteredList = listItem.filter(item => item.id !== id)
-    setAndSaveItem(filteredList)
-  }
+    const filteredList = listItem.filter((item) => item.id !== id);
+    setListItem(filteredList);
+  };
 
   const handleCheck = (id) => {
     const checkedItems = listItem.map((item) =>
-       item.id === id ? {...item, checked: !item.checked} : item)
-    setAndSaveItem(checkedItems)
-  }
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setListItem(checkedItems);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!inputValue) return;
-    addItem(inputValue);    
+    addItem(inputValue);
     setInputValue("");
   };
 
   return (
     <div className="container">
       <Header />
-      <Form inputValue={inputValue} setInputValue={setInputValue} handleSubmit={handleSubmit} />
-      <List listItem={listItem} deleteItem={deleteItem} handleCheck={handleCheck}/>
+      <Form
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        handleSubmit={handleSubmit}
+      />
+      <List
+        listItem={listItem}
+        deleteItem={deleteItem}
+        handleCheck={handleCheck}
+      />
     </div>
   );
 }
